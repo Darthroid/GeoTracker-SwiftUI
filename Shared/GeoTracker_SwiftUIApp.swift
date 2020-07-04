24eb67fe3d967b3ba1fa6ea7aa8e2430bf8052b1
Oleg Komaristy <darthroid@gmail.com>
2020-07-04 09:38:46 +0300

    Initial Commit

---
+++ Shared/GeoTracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoTracker_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Oleg Komaristy on 04.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct GeoTracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
